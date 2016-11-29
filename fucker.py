28c6846878028fd504751e24e4e0577d5e6230a6
Adi Ron <adi@adiron.me>
2016-11-29 18:55:38 +0200

    Added the option to only output the final result

--- fucker.py
+++ fucker.py
@@ -17,6 +17,7 @@ def main():
     parser.add_argument('--seed', '-s', nargs=1, type=int, default=[0], help='set the random seed if needed')
     parser.add_argument('--write', '-w', action="store_true", help="write out files (default is stdout)")
     parser.add_argument('--path', '-p', nargs=1, type=str, default=['.'], help="write path when using writing files")
+    parser.add_argument('--final', action="store_true", help="write only the final file")
     args = parser.parse_args()
 
     if args.seed[0] != 0:
@@ -40,10 +41,11 @@ def fuck(data, args, carry=0):
     data = data[:s] + garbage + data[s:]
 
     if args.write:
-        fn = path.join(args.path[0], 'fucked-{:06d}-{}'.format(args.generations[0] - carry, path.basename(args.file[0].name)))
-        with open(fn, 'wb+') as f:
-            print('Writing to {}...'.format(fn))
-            f.write(data)
+        if (args.final and carry == 1) or not args.final:
+            fn = path.join(args.path[0], 'fucked-{:06d}-{}'.format(args.generations[0] - carry, path.basename(args.file[0].name)))
+            with open(fn, 'wb+') as f:
+                print('Writing to {}...'.format(fn))
+                f.write(data)
     if carry > 1:
         return fuck(data, args, carry - 1)
     else:
